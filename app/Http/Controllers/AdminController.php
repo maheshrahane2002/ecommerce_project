7639cbe8d6c37c2ba9c39c84b3c3ad10e4034499
Mahesh Rahane <maheshrahane65@gmail.com>
2024-06-20 11:13:34 +0530

    To upload product data to the database.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Category;
+use App\Models\Product;
 use Flasher\Laravel\Facades\Flasher;
 
 class AdminController extends Controller
@@ -50,4 +51,31 @@ class AdminController extends Controller
         toastr()->timeOut(10000)->closeButton()->addSuccess('Category Updated Successfully..!');
         return redirect('/view_category');
     }
+
+    public function add_product()
+    {
+        $category = Category::all();
+        return view('admin.add_product',compact('category'));
+   }
+
+   public function upload_product(Request $request)
+   {
+       $data = new Product;
+       $data->title = $request->title;
+       $data->description = $request->description;
+       $data->price = $request->price;
+       $data->quantity = $request->qty;
+       $data->category = $request->category;
+ 
+       $image = $request->image;
+       if($image){
+        $imagename = time().'.'.$image->getClientOriginalExtension();
+        $request->image->move('products',$imagename);
+        $data->image = $imagename;
+       }
+
+       $data->save();
+       toastr()->timeOut(10000)->closeButton()->addSuccess('Product Added Successfully..!');
+       return redirect()->back();
+   }
 }
