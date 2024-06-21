86ceb5a3beb9db277ab1ee6b20bdbb9c16a364b2
Mahesh Rahane <maheshrahane65@gmail.com>
2024-06-21 15:51:39 +0530

    To display products data in home page.

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,12 @@ use App\Http\Controllers\AdminController;
 
 Route::get('/',[HomeController::class,'home']);
 
-Route::get('/dashboard', function () {
-    return view('home.index');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard',[HomeController::class,'login_home'])->middleware(['auth', 'verified'])->name('dashboard');
+
+
+
+
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
