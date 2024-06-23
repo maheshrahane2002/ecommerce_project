b49160c148751eeba7a1a8d87f3f1cfb3d33b40f
Mahesh Rahane <maheshrahane65@gmail.com>
2024-06-23 09:48:33 +0530

    To display product details in Laravel.

--- resources/views/home/product.blade.php
+++ resources/views/home/product.blade.php
@@ -20,7 +20,6 @@
 
         <div class="col-sm-6 col-md-4 col-lg-3">
           <div class="box">
-            <a href="">
               <div class="img-box">
                 <img src="products/{{$products->image}}" alt="">
               </div>
@@ -30,7 +29,9 @@
                   <span>{{$products->price}}</span>
                 </h6>
               </div>
-              </a>
+              <div style="padding: 15px;">
+                <a class="btn btn-danger" href="{{url('product_details',$products->id)}}">Details</a>
+              </div>
           </div>
         </div>
 
--- routes/web.php
+++ routes/web.php
@@ -49,6 +49,9 @@ route::post('edit_product/{id}',[AdminController::class,'edit_product'])->middle
 
 route::get('product_search',[AdminController::class,'product_search'])->middleware(['auth','admin']);
 
+route::get('product_details/{id}',[HomeController::class,'product_details']);
+
+
 
 
 
