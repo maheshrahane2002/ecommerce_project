072a83b26799541d963cf546f4e14ad45d561da0
Mahesh Rahane <maheshrahane65@gmail.com>
2024-06-23 10:55:19 +0530

    To add product to Cart in Laravel.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,12 @@ use Illuminate\Http\Request;
 
 use App\Models\Product;
 
+use App\Models\User;
+
+use App\Models\Cart;
+
+use Illuminate\Support\Facades\Auth;
+
 class HomeController extends Controller
 {
     public function index()
@@ -28,7 +34,19 @@ class HomeController extends Controller
     public function product_details($id)
     {
         $data = Product::find($id);
-
         return view('home.product_details',compact('data'));
+    }
+
+    public function add_cart($id)
+    {
+        $product_id = $id;
+        $user=Auth::user();
+        $user_id=$user->id;
+        $data=new Cart;
+        $data->user_id=$user_id;
+        $data->product_id=$product_id;
+        $data->save();
+        toastr()->timeOut(10000)->closeButton()->addSuccess('Product Added to the Cart Successfully..!');
+        return redirect()->back();
     }
 }
