0fdcf665fadf604851c84867a870f1b719fc8fc0
Mahesh Rahane <maheshrahane65@gmail.com>
2024-06-24 09:31:47 +0530

    To move cart data to order table.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,6 +10,8 @@ use App\Models\User;
 
 use App\Models\Cart;
 
+use App\Models\Order;
+
 use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
@@ -93,6 +95,33 @@ class HomeController extends Controller
             toastr()->timeOut(10000)->closeButton()->addSuccess('Product Removed from the Cart Successfully..!');
         return redirect()->back();
     }
-    
 
+    public function confirm_order(Request $request)
+    {
+       $name = $request->name;
+       $address = $request->address;
+       $phone = $request->phone;
+       $userid = Auth::user()->id;
+       $cart = Cart::where('user_id',$userid)->get();
+
+       foreach($cart as $carts){
+       $order = new Order;
+       $order->name=$name;
+       $order->rec_address=$address;
+       $order->phone=$phone;
+       $order->user_id=$userid;
+       $order->product_id=$carts->product_id;
+       $order->save();
+       }
+       $cart_remove=Cart::where('user_id',$userid)->get();
+       foreach($cart_remove as $remove)
+       {
+        $data = Cart::find($remove->id);
+        $data->delete();
+       } 
+       toastr()->timeOut(10000)->closeButton()->addSuccess('Product Ordered Successfully..!');
+
+    return redirect()->back();
+
+    }
 }
