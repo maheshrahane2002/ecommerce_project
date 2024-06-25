e3aed5810bfc0d093a4aa34ecf5b8d12e5c1bc07
Mahesh Rahane <maheshrahane65@gmail.com>
2024-06-25 20:17:41 +0530

    To customize login and register page design.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,20 @@
-<x-guest-layout>
+<!DOCTYPE html>
+<html>
+
+<head>
+  @include('home.css')
+</head>
+
+<body>
+  <div class="hero_area">
+        <!-- header section strats -->
+
+         @include('home.header')
+
+        <!-- end header section -->
+
+
+        <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
@@ -45,3 +61,14 @@
         </div>
     </form>
 </x-guest-layout>
+
+  
+
+
+   
+
+  
+
+</body>
+
+</html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,18 @@
-<x-guest-layout>
+<!DOCTYPE html>
+<html>
+
+<head>
+  @include('home.css')
+</head>
+
+<body>
+  <div class="hero_area">
+        <!-- header section strats -->
+
+         @include('home.header')
+
+        <!-- end header section -->
+        <x-guest-layout>
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
@@ -64,3 +78,15 @@
         </div>
     </form>
 </x-guest-layout>
+
+
+        
+
+
+
+   
+
+  
+</body>
+
+</html>
